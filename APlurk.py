4377d28a9fcb5814331af2f631cb001efee0e6c6
itex3z <itex3z@f4ce89d0-285b-11df-b6a2-4f9457dbae47>
2010-03-05 14:21:18 +0000

    Добавлены уведомляющие окна

--- APlurk.py
+++ APlurk.py
@@ -1,6 +1,9 @@
 ﻿from graphics import *
 import urllib, urllib2, cookielib, e32, appuifw
 
+
+
+
 #Background
 appuifw.app.screen='large' #Screen size(large)
 img=Image.new((240,320)) #background
@@ -11,11 +14,6 @@ def handle_redraw(rect):
 canvas=appuifw.Canvas(event_callback=None, redraw_callback=handle_redraw)
 appuifw.app.body=canvas
 
-#Plurk settings
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-plurk_api_key = '5cZQnLGHJMmPRYsADiOq1x1wMuSkmocE'
-get_api_url = lambda x: 'http://www.plurk.com/API%s' % x
-encode = urllib.urlencode
 
 #Menu functions
 def send_message():
@@ -29,26 +27,25 @@ def quit():
     app_lock.signal()
 
 def post_to_plurk(pmessage):
-	if pmessage == None:
-		pass
-	else:
-		appuifw.note(u"Text "+ pmessage, "info")
-		try:
-			fp = opener.open(get_api_url('/Users/login'),
-				encode({'username': 's60test',
-						'password': '1097',
-						'api_key': plurk_api_key}))
-			print fp.read()
-		except urllib2.URLError:
-			print 'error with login'
-		try:
-			fp = opener.open(get_api_url('/Timeline/plurkAdd'),
-				encode({'content':pmessage.encode('utf-8'),
-						'qualifier': 'says',
-						'api_key': plurk_api_key}))
-			print fp.read()
-		except urllib2.URLError:
-			print 'error with adding'
+	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+	plurk_api_key = '5cZQnLGHJMmPRYsADiOq1x1wMuSkmocE'
+	get_api_url = lambda x: 'http://www.plurk.com/API%s' % x
+	encode = urllib.urlencode
+	try:
+		fp = opener.open(get_api_url('/Users/login'),
+			encode({'username': 's60test',
+					'password': '1097',
+					'api_key': plurk_api_key}))		
+	except urllib2.URLError:
+		appuifw.note(u"Logon Error", "error")
+	try:
+		fp = opener.open(get_api_url('/Timeline/plurkAdd'),
+			encode({'content':pmessage.encode('utf-8'),
+					'qualifier': 'says',
+					'api_key': plurk_api_key}))
+		appuifw.note(u"Posted!", "conf")
+	except urllib2.URLError:
+		appuifw.note(u"Posting Error", "error")
 #Menu list
 appuifw.app.menu = [(u"Send Message", send_message),
                     (u"About", about),
