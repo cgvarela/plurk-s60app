34fafe63a3159b4b021544b500420e83f747bcdd
xollvo <xollvo@f4ce89d0-285b-11df-b6a2-4f9457dbae47>
2010-03-10 18:40:29 +0000

    Fixed -- Access point -- Picture preview
    Added
    -- Form for plurk posting (with qualifiers and long text support)
    
    Preview process was optimized

--- APlurk.py
+++ APlurk.py
@@ -4,30 +4,34 @@ import urllib, urllib2, cookielib, e32, appuifw, e32dbm, os
 
 plurk_login = u'Plurk User'
 plurk_password = 'None'
-path = u"C:\\data\python\\"
 limg = None
 limg_path = u''
 
-
 #Main Part
+# import graphics
+# print graphics.rect
 
 #Background
 if not appuifw.touch_enabled():
+	path = u"c:\\data\python\\"
 	appuifw.app.screen='normal' #Screen size(large)
 	img=Image.new((240,320)) #background
-	bgimage=Image.open(path+"test.png") #Image path should be like C:\\data\Images\\test.jpg
+	bgimage=Image.open(path+"test.jpg") #Image path should be like C:\\data\Images\\test.jpg
 	appuifw.app.title=u'Plurk App'
 else:
+	path = u"e:\\data\python\\"
 	appuifw.app.directional_pad = False
 	appuifw.app.screen='normal' #Screen size(normal)
 	img=Image.new((360,640)) #background
 	bgimage=Image.open(path+"test_b.jpg") #Image path should be like C:\\data\Images\\test.jpg
 	appuifw.app.title=u'Plurk App'
 
+def handle_redraw(rect = None):
+	''' rect is a four-element tuple
+	ex. (0, 0, 360, 487) '''
+	canvas.blit(bgimage)
 
-def handle_redraw(rect):
-	canvas.blit(bgimage) #Drawing background
-canvas = appuifw.Canvas(event_callback=None, redraw_callback=handle_redraw)
+canvas = appuifw.Canvas(event_callback = None, redraw_callback = handle_redraw)
 appuifw.app.body = canvas
 
 #Work with file
@@ -37,7 +41,6 @@ def write():  #define the write function to write in a database
 	db[u"login"] = plurk_login
 	db[u"password"] = plurk_password
 	db.close()
-
 
 def read():  #define a read function to read a database
 	global plurk_login, plurk_password
@@ -66,7 +69,7 @@ def quit():
 	app_lock.signal()
 	write()
 
-def post_to_plurk(pmessage):
+def post_to_plurk():
 	global plurk_login, plurk_password
 	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
 	plurk_api_key = '5cZQnLGHJMmPRYsADiOq1x1wMuSkmocE'
@@ -81,8 +84,8 @@ def post_to_plurk(pmessage):
 		appuifw.note(u"Logon Error", "error")
 	try:
 		fp = opener.open(get_api_url('/Timeline/plurkAdd'),
-			encode({'content':pmessage.encode('utf-8'),
-					'qualifier': 'says',
+			encode({'content': plurk_text.encode('utf-8'),
+					'qualifier': qualifier,
 					'api_key': plurk_api_key}))
 		appuifw.note(u"Posted!", "conf")
 	except urllib2.URLError:
@@ -102,16 +105,46 @@ def add_pic_filesystem():
 		if os.path.splitext(x)[1] in ('.jpg', '.png', '.gif'):
 			images.append(x)
 	
-	index = appuifw.popup_menu(images)#selection_list(images)
+	index = appuifw.popup_menu(images)
 	limg = Image.open(imagedir + '\\' + images[index])
 	limg_path = imagedir + '\\' + images[index]
+	
+	# Resize image
+	if limg.size[0] > 360:
+		limg = limg.resize((240, 400), callback = None, keepaspect = 1)
+	
+	# Optimize UI redraw
+	canvas.begin_redraw()
+	handle_redraw()
+	canvas.end_redraw()
+	
 	# Display image in the middle of screen
 	canvas.blit(limg, target = ((canvas.size[0] - limg.size[0])/2, (canvas.size[1] - limg.size[1])/2))
-
+	
 def add_pic_photocamera():
 	pass
 
+plurk_text = u'plurk text'
+qualifier = u':'
+qualifiers = [u'loves', u'likes', u'shares', u'gives', u'hates', u'wants', u'has', u'will', u'asks', u'wishes',
+            u'was', u'feels', u'thinks', u'says', u'is', u':', u'freestyle', u'hopes', u'needs', u'wonders']
+def save_form(arg):
+   global plurk_text, qualifier
+   qualifier = qualifiers[arg[0][2][1]]
+   plurk_text = arg[1][2]
+   print qualifier + ' ' + plurk_text
+
+def add_text():
+   global plurk_text, qualifiers
+   fields = [(u'Qualifier', 'combo', (qualifiers, 0)), (u'Text', 'text', plurk_text)]
+   myForm = appuifw.Form(fields, flags=appuifw.FFormEditModeOnly)
+   # Assign the save function
+   myForm.save_hook = save_form
+   # Execute the form
+   myForm.execute()
+
 def select_access_point():
+	''' Return True if selected, False otherwise '''
 	import btsocket
 	
 	pnts = []
@@ -122,30 +155,28 @@ def select_access_point():
 	index = appuifw.popup_menu(pnts, u'Select default access point:')
 	if index is not None:
 		set_default_access_point(pnts[index])
-		return 1;
+		return True;
+	
+	return False
 
-while select_access_point()!=1:
-	select_access_point()
-
+while not select_access_point():
+	pass
 
 if os.path.exists(path+"settings.db.e32dbm"):
 	read()
 else:
 	settings()
-
 		
 #Menu list
-appuifw.app.menu = [(u"Plurk!", send_message),
+appuifw.app.menu = [(u"Plurk!", post_to_plurk),
 					(u'Add picture',
 						((u'From Gallery', add_pic_filesystem),
 						(u'With your camera', add_pic_photocamera))
 					),
+					(u'Add text', add_text),
 					(u"Settings", settings),
-					(u"About", about),
-					(u"Quit", quit)]
+					(u"About", about)]
 
 appuifw.app.exit_key_handler = quit # exit
 app_lock = e32.Ao_lock()   #Exit
 app_lock.wait() #prevent the application from closing before the user tells it to
-
-
